9d507f671f60276ec2d39a3363f85d592146459f
Daniel Muszkiet <danielmuszkiet.marketing@gmail.com>
2025-05-29 15:12:55 +0200

    refactor: simplify className logic in Options component

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -12,13 +12,21 @@ function Options({ question, dispatch, answer }: OptionProps) {
   return (
     <div className="options">
       {question.options.map((option, i) => {
+        const isSelected = i === answer;
+        const isCorrect = i === question.correctOption;
+
+        let statusClass = "";
+        if (hasAnswered) {
+          statusClass = isCorrect ? "correct" : "wrong";
+        }
+
+        const answerClass = isSelected ? "answer" : "";
+
         return (
           <button
             disabled={hasAnswered}
             key={question.id + "-" + i}
-            className={`btn btn-option  
-              ${hasAnswered ? (i === question.correctOption ? "correct" : "wrong") : ""}
-              ${i === answer ? "answer" : ""}`}
+            className={`btn btn-option ${statusClass} ${answerClass}`}
             onClick={() => dispatch({ type: "newAnswer", payload: i })}
           >
             {option}
